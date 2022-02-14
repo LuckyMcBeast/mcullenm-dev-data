cadd25d60b7ad5b5c12a0a518bf3cf6270d80c46
Cullen McClellan <cullenmcclellan@gmail.com>
2022-02-13 19:56:32 -0500

    [MCMD-18] updates startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
-echo "Initializing  database container..."
+echo "Starting database container..."
 docker start mcullenm_dev_db
 echo "Starting Python Virtual Enviroment..."
 source venv/bin/activate
 echo "Starting Uvicorn Server..."
 nohup uvicorn src.main:app --reload &
+sleep 1
 echo "Done."
