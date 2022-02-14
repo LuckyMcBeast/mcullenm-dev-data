3cd34f5cb25164ade50e4427428e554113a45793
Cullen McClellan <cullenmcclellan@gmail.com>
2022-02-13 20:30:09 -0500

    [MCMD-18] adds requirements check to startup

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@ echo "Starting database container..."
 docker start mcullenm_dev_db
 echo "Starting Python Virtual Enviroment..."
 source venv/bin/activate
+echo "Checking for Requirements..."
+pip3 install -r requirements.txt
 echo "Starting Uvicorn Server..."
 nohup uvicorn src.main:app --reload &
 sleep 1
